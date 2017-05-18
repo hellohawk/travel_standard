a6c897333c108d45c4ed9fba79ab88df9e1d801c
Shuhei Hatori <shuhei.hatori@gmail.com>
2017-05-18 20:54:36 +0900

    add maps csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,16 @@ require "csv"
 #     Like.create(user_id: row[3], upload_id: row[4])
 #   end
 
-searches_csv = CSV.readlines("db/searches.csv")
-searches_csv.shift
+# searches_csv = CSV.readlines("db/searches.csv")
+# searches_csv.shift
 
+#   searches_csv.each do |row|
+#      Search.create(upload_id: row[1], country_name: row[4], city_name: row[5], conti_name: row[6], subconti_name: row[7], country: row[8], city: row[9], continent: row[10], subcontinent: row[11])
+#    end
 
+maps_csv = CSV.readlines("db/maps.csv")
+maps_csv.shift
 
-  searches_csv.each do |row|
-     Search.create(upload_id: row[1], country_name: row[4], city_name: row[5], conti_name: row[6], subconti_name: row[7], country: row[8], city: row[9], continent: row[10], subcontinent: row[11])
+  maps_csv.each do |row|
+     Map.create(resolutions: row[3], regions: row[4], regions_name: row[5], information: row[6], pre_resolutions: row[7])
    end
