ba24a407ed18fe489f0b86741a4bf65e7b104aa2
Shuhei Hatori <shuhei.hatori@gmail.com>
2017-05-18 20:13:05 +0900

    ganbaru

--- db/migrate/20170510184630_add_column_to_searches.rb
+++ db/migrate/20170510184630_add_column_to_searches.rb
@@ -1,17 +1,15 @@
 class AddColumnToSearches < ActiveRecord::Migration
   def change
-
-    add_column :searches, :city, :string
-    add_column :searches, :city_code, :string
-    add_column :searches, :country, :string
-    add_column :searches, :country_code, :string
-    add_column :searches, :area, :string
-    add_column :searches, :area_code, :string
-    add_column :searches, :tour_count, :integer
-
-    remove_column :searches, :area_id, :integer
-    remove_column :searches, :country_id, :integer
-    remove_column :searches, :city_id, :integer
-
+    create_table :Searches do |t|
+        t.string :upload_id
+        t.string :city
+        t.string :city_code
+        t.string :country
+        t.string :country_code
+        t.string :area
+        t.string :area_code
+        t.integer :tour_count
+        t.timestamps null: false
+    end
   end
 end
