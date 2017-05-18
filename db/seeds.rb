1ae9cfc8be9b2d8b47eabb4573d787c6fde97196
Shuhei Hatori <shuhei.hatori@gmail.com>
2017-05-18 21:02:10 +0900

    re add maps csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,16 +21,16 @@ require "csv"
 #      Search.create(upload_id: row[1], country_name: row[4], city_name: row[5], conti_name: row[6], subconti_name: row[7], country: row[8], city: row[9], continent: row[10], subcontinent: row[11])
 #    end
 
-# maps_csv = CSV.readlines("db/maps.csv")
-# maps_csv.shift
+maps_csv = CSV.readlines("db/maps.csv")
+maps_csv.shift
 
-#   maps_csv.each do |row|
-#      Map.create(resolutions: row[3], regions: row[4], regions_name: row[5], information: row[6], pre_resolutions: row[7])
-#    end
+  maps_csv.each do |row|
+     Map.create(resolutions: row[3], regions: row[4], regions_name: row[5], information: row[6], pre_resolutions: row[7])
+   end
 
-uploads_csv = CSV.readlines("db/uploads.csv")
-uploads_csv.shift
+# uploads_csv = CSV.readlines("db/uploads.csv")
+# uploads_csv.shift
 
-  uploads_csv.each do |row|
-     Upload.create(image: row[3], comment: row[4], user_id: row[5], likes_count: row[6], search_id: row[7], continent: row[8], subcontinent: row[9], country: row[10], city: row[11], map_id: row[12])
-  end
+#   uploads_csv.each do |row|
+#      Upload.create(image: row[3], comment: row[4], user_id: row[5], likes_count: row[6], search_id: row[7], continent: row[8], subcontinent: row[9], country: row[10], city: row[11], map_id: row[12])
+#   end
