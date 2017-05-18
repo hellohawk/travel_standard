351f791a8a0515cffbc0cf1cc2606d9a98857c59
Shuhei Hatori <shuhei.hatori@gmail.com>
2017-05-18 20:51:12 +0900

    add searches csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,17 @@
 
 require "csv"
 
-likes_csv = CSV.readlines("db/likes.csv")
-likes_csv.shift
-  likes_csv.each do |row|
-    Like.create(user_id: row[3], upload_id: row[4])
-  end
+# likes_csv = CSV.readlines("db/likes.csv")
+# likes_csv.shift
+#   likes_csv.each do |row|
+#     Like.create(user_id: row[3], upload_id: row[4])
+#   end
+
+searches_csv = CSV.readlines("db/searches.csv")
+searches_csv.shift
+
+
+
+  searches_csv.each do |row|
+     Search.create(upload_id: row[1], country_name: row[4], city_name: row[5], conti_name: row[6], subconti_name: row[7], country: row[8], city: row[9], continent: row[10], subcontinent: row[11])
+   end
